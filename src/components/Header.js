429e0c772695f0db76e1912b575d879b65019bc2
vijay <anegondivijay9490@gmail.com>
2025-06-14 11:17:25 +0530

    cube added for photo show

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,24 @@ import '../App.css';
 const Header = () => {
   return (
     <header id="header" className="header">
-      <div className="photoBoxWrapper">
-        <div className="photoBox">
-          <img src="/IMG_20240115_123909.jpg" alt="Anegondi Vijay" className="photo" />
-        </div>
+      <div className="img111">
+        
+          
+        <div className="scene">
+      <div className="cube">
+        <div className="face front"><img src="/IMG_20240115_123909.jpg" alt="front" className="photo" /></div>
+        <div className="face back"><img src="/IMG_20240115_123909.jpg" alt="back" className="photo" /></div>
+        <div className="face right"><img src="/IMG_20240115_123909.jpg" alt="right" className="photo" /></div>
+        <div className="face left"><img src="/IMG_20240115_123909.jpg" alt="left" className="photo" /></div>
+        <div className="face top"><img src="/IMG_20240115_123909.jpg" alt="top" className="photo" /></div>
+        <div className="face bottom"><img src="/IMG_20240115_123909.jpg" alt="bottom" className="photo" /></div>
       </div>
+    </div>
+      </div><br/>
+      <div className='nametitle1'>
       <h1 className="name">Anegondi Vijay</h1>
       <p className="title">Web Developer Portfolio</p>
+      </div>
     </header>
   );
 };
